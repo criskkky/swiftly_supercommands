9414a4ce0b04b953db5a8c27ea55842a8305998d
criskkky <36176343+criskkky@users.noreply.github.com>
2024-11-12 22:53:58 -0300

    Update commands.lua

--- src/plugins/supercommands/commands.lua
+++ src/plugins/supercommands/commands.lua
@@ -1,105 +1,113 @@
 commands:Register("hp", function(playerid, args, argsCount, silent, prefix)
-    -- Server Logic
+    local admin = nil
+    
     if playerid == -1 then
-        if argsCount < 2 then
-            return ReplyToCommand(playerid, config:Fetch("admins.prefix"), FetchTranslation("supercommands.hp.usage"))
+        -- Set admin name to CONSOLE if executed by the server console
+        admin = "CONSOLE"
+    else
+        -- Set admin name to the player name if executed by a player
+        local player = GetPlayer(playerid)
+        if not player then return end
+
+        local hasAccess = exports["admins"]:HasFlags(playerid, "c")
+    
+        -- Permission Check
+        if not hasAccess then
+            return ReplyToCommand(playerid, config:Fetch("admins.prefix"), FetchTranslation("admins.no_permission"))
         end
 
-        local players = FindPlayersByTarget(args[1], true)
-        if #players == 0 then
-            return ReplyToCommand(playerid, config:Fetch("admins.prefix"), FetchTranslation("admins.invalid_player"))
+        if player:IsValid() then
+            admin = player:CBasePlayerController().PlayerName
         end
+    end
+    
+    if argsCount < 2 then
+        return ReplyToCommand(playerid, config:Fetch("admins.prefix"), FetchTranslation("supercommands.hp.usage"))
+    end
 
-        local health = tonumber(args[2])
-        if not health then
-            return ReplyToCommand(playerid, config:Fetch("admins.prefix"), FetchTranslation("supercommands.hp.invalid_health"))
-        end
+    local players = FindPlayersByTarget(args[1], true)
+    if #players == 0 then
+        return ReplyToCommand(playerid, config:Fetch("admins.prefix"), FetchTranslation("admins.invalid_player"))
+    end
 
-        local armor = nil
-        if argsCount >= 3 then
-            armor = tonumber(args[3])
+    local health = tonumber(args[2])
+    if not health then
+        return ReplyToCommand(playerid, config:Fetch("admins.prefix"), FetchTranslation("supercommands.hp.invalid_health"))
+    end
+
+    local armor = nil
+    if argsCount >= 3 then
+        armor = tonumber(args[3])
             if not armor then
                 return ReplyToCommand(playerid, config:Fetch("admins.prefix"), FetchTranslation("supercommands.hp.invalid_armor"))
             end
-        end
+    end
 
-        local helmet = nil
-        if argsCount >= 4 then
-            helmet = tonumber(args[4])
+    local helmet = nil
+    if argsCount >= 4 then
+        helmet = tonumber(args[4])
             if not helmet then
                 return ReplyToCommand(playerid, config:Fetch("admins.prefix"), FetchTranslation("supercommands.hp.invalid_helmet"))
             end
-        end
+    end
 
-        for i = 1, #players do
+    for i = 1, #players do
             local pl = players[i]
             pl:CBaseEntity().Health = health
             pl:CBaseEntity().Armor = armor or pl:CBaseEntity().Armor
             pl:CBaseEntity().Helmet = helmet or pl:CBaseEntity().Helmet
 
             if pl:CBaseEntity().Health <= 0 then
-                pl:Kill()
-            end
+            pl:Kill()
         end
+    end
 
-        -- Enviar mensaje único para múltiples jugadores
-        local message = nil
-        if #players > 1 then
-            if argsCount == 2 then
-                -- Solo se aplica la salud
-                message = FetchTranslation("supercommands.hp.mult_message")
-                    :gsub("{ADMIN_NAME}", "CONSOLE")
-                    :gsub("{PLAYER_COUNT}", tostring(#players))
-                    :gsub("{HEALTH}", tostring(health))
-                elseif argsCount == 3 then
-                    -- Salud y armadura
-                    message = FetchTranslation("supercommands.hp.mult_message_with_armor")
-                    :gsub("{ADMIN_NAME}", "CONSOLE")
-                    :gsub("{PLAYER_COUNT}", tostring(#players))
-                    :gsub("{HEALTH}", tostring(health))
-                    :gsub("{ARMOR}", tostring(armor))
-                elseif argsCount == 4 then
-                    -- Salud, armadura y casco
-                    message = FetchTranslation("supercommands.hp.mult_message_with_helmet")
-                    :gsub("{ADMIN_NAME}", "CONSOLE")
-                    :gsub("{PLAYER_COUNT}", tostring(#players))
-                    :gsub("{HEALTH}", tostring(health))
-                    :gsub("{ARMOR}", tostring(armor))
-                    :gsub("{HELMET}", tostring(helmet))
+    -- Message handling for multiple players
+    local message = nil
+    if #players > 1 then
+        if argsCount == 2 then
+            message = FetchTranslation("supercommands.hp.mult_message")
+            :gsub("{ADMIN_NAME}", admin)
+            :gsub("{PLAYER_COUNT}", tostring(#players))
+            :gsub("{HEALTH}", tostring(health))
+        elseif argsCount == 3 then
+            message = FetchTranslation("supercommands.hp.mult_message_with_armor")
+            :gsub("{ADMIN_NAME}", admin)
+            :gsub("{PLAYER_COUNT}", tostring(#players))
+            :gsub("{HEALTH}", tostring(health))
+            :gsub("{ARMOR}", tostring(armor))
+            elseif argsCount == 4 then
+                message = FetchTranslation("supercommands.hp.mult_message_with_helmet")
+                :gsub("{ADMIN_NAME}", admin)
+                :gsub("{PLAYER_COUNT}", tostring(#players))
+                :gsub("{HEALTH}", tostring(health))
+                :gsub("{ARMOR}", tostring(armor))
+                :gsub("{HELMET}", tostring(helmet))
             end
             ReplyToCommand(playerid, config:Fetch("admins.prefix"), message)
         else
-            -- Enviar mensaje individual para un solo jugador
+            -- Message handling for single player
             local pl = players[1]
             if argsCount == 2 then
                 message = FetchTranslation("supercommands.hp.message")
-                    :gsub("{ADMIN_NAME}", "CONSOLE")
-                    :gsub("{PLAYER_NAME}", pl:CBasePlayerController().PlayerName)
-                    :gsub("{HEALTH}", tostring(health))
+                :gsub("{ADMIN_NAME}", admin)
+                :gsub("{PLAYER_NAME}", pl:CBasePlayerController().PlayerName)
+                :gsub("{HEALTH}", tostring(health))
             elseif argsCount == 3 then
                 message = FetchTranslation("supercommands.hp.message_with_armor")
-                    :gsub("{ADMIN_NAME}", "CONSOLE")
-                    :gsub("{PLAYER_NAME}", pl:CBasePlayerController().PlayerName)
-                    :gsub("{HEALTH}", tostring(health))
-                    :gsub("{ARMOR}", tostring(armor))
+                :gsub("{ADMIN_NAME}", admin)
+                :gsub("{PLAYER_NAME}", pl:CBasePlayerController().PlayerName)
+                :gsub("{HEALTH}", tostring(health))
+                :gsub("{ARMOR}", tostring(armor))
             elseif argsCount == 4 then
                 message = FetchTranslation("supercommands.hp.message_with_helmet")
-                    :gsub("{ADMIN_NAME}", "CONSOLE")
-                    :gsub("{PLAYER_NAME}", pl:CBasePlayerController().PlayerName)
-                    :gsub("{HEALTH}", tostring(health))
-                    :gsub("{ARMOR}", tostring(armor))
-                    :gsub("{HELMET}", tostring(helmet))
+                :gsub("{ADMIN_NAME}", admin)
+                :gsub("{PLAYER_NAME}", pl:CBasePlayerController().PlayerName)
+                :gsub("{HEALTH}", tostring(health))
+                :gsub("{ARMOR}", tostring(armor))
+                :gsub("{HELMET}", tostring(helmet))
             end
             ReplyToCommand(playerid, config:Fetch("admins.prefix"), message)
-        end
-    else
-        -- Player Logic (to be implemented)
-        local player = GetPlayer(playerid)
-        if not player then return end
-
-        -- Permission Check
-        if not hasAccess then
-            return ReplyToCommand(playerid, config:Fetch("admins.prefix"), FetchTranslation("admins.no_permission"))
         end
     end
 end)
